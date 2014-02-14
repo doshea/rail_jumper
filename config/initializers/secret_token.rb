874293598703e64f9f995d53226b2d8f1ea8e698
Dylan O'Shea <dylan.j.oshea@gmail.com>
2014-02-14 00:37:51 -0800

    Initializing project without TestUnit and with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailJumper4::Application.config.secret_key_base = '42666fc91b594cf169d4e7b4d9d446f51bec3e9bc39452fcdab05e1eec000ffc0466bf9df12d5f3e8963c81780c26933c8cdc64bf407693f00aed5db06df10ba'
