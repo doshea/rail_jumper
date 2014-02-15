60006dda36554b0e4ae1eef58d50b04c753d1ed1
Dylan O'Shea <dylan.j.oshea@gmail.com>
2014-02-15 07:36:42 -0800

    Rearranging routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 RailJumper4::Application.routes.draw do
   root to: 'pages#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/login' => 'sessions#destroy'
+
   resources :users, only: [:new, :create]
+  
   namespace :account do
     get '/', to: :show
     patch :update
     patch :change_password
   end
-
-
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/login' => 'sessions#destroy'
 
   namespace :admin do
     get '/', to: :index
